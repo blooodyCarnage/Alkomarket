9825dbe023ed81fa0ef83ad573d1a955a25b350b
blooodyCarnage <smash100pro@gmail.com>
2026-03-20 00:31:24 +0700

    z

--- Alkomarket/alko/urls.py
+++ Alkomarket/alko/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, register_converter
 from alko import views
+from alko import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
+
 urlpatterns = [
- path('', views.index),
+    path('', views.index, name='home'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path('archive/<year4:year>/', views.archive, name='archive'),
 ]
---
+++ Alkomarket/alko/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponseNotFound
+
+def index(request):
+    return HttpResponse("Главная страница")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    if year > 2023:
+        return redirect('home')
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
